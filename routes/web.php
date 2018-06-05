834101f0a8d0c3b3a341c76696f1426965fbbca2
Andre <andre.webderking@gmail.com>
2018-06-05 16:27:15 -0300

    Working With the Query Builder

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tasks', function () {
+
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks.index', compact('tasks'));
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/tasks/{task}', function ($id) {
+
+    $task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
 });
