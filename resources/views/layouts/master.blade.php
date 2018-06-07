86c1c0abf355a15173b6373445986df687f949ba
Andre <andre.webderking@gmail.com>
2018-06-07 14:03:40 -0300

    Laravel Mix and the Front-end

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="/css/blog.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
 
