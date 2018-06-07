c9a29922e137fc06c5e126f99d841c06206b7326
Andre <andre.webderking@gmail.com>
2018-06-07 16:49:35 -0300

    Associating With Users

--- app/Comment.php
+++ app/Comment.php
@@ -5,6 +5,14 @@ namespace App;
 class Comment extends Model
 {
     public function post() {
+
         return $this->belongsTo(Post::class);
+
+    }
+
+    public function user() {
+
+        return $this->belongsTo(User::class);
+        
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -5,7 +5,15 @@ namespace App;
 class Post extends Model
 {
     public function comments() {
+
         return $this->hasMany(Comment::class);
+        
+    }
+
+    public function user() {
+
+        return $this->belongsTo(User::class);
+
     }
 
     public function addComment($body) {
