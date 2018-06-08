3e7a0b2c3ecd6f74385bf220500483455136c1ff
Andre <andre.webderking@gmail.com>
2018-06-08 17:21:12 -0300

    View Composers

--- app/Post.php
+++ app/Post.php
@@ -41,4 +41,18 @@ class Post extends Model
 
     }
 
+    public static function archives() {
+
+        return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            
+        ->groupBy('year', 'month')
+
+        ->orderByRaw('min(created_at) desc')
+
+        ->get()
+        
+        ->toArray();
+
+    }
+
 }
